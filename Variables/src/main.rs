8a4bc35672308551e4095eef9f7a5dfee3e16612
SoulNaturalist <omg12121@mail.ru>
2023-01-23 02:37:02 +0300

    added Variables theme

---
+++ Variables/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let user_have_skill: bool = true;
+    let example_int: u16 = 10;
+    let example_float: f32 = 10.11;
+    let name = "Dirk";
+    let example_str = format!("Hello {name}");
+
+    println!("{example_int} & {example_float}");
+    println!("that bool {user_have_skill}");
+    println!("{example_str}")
+
+}
