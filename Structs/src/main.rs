5a6d002754a4535fd69b13573b50001d21918a87
SoulNaturalist <omg12121@mail.ru>
2023-01-27 06:59:39 +0300

    added theme sructs

---
+++ Structs/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    struct user {
+        id: i32,
+        data_registartion: String,
+        username: String,
+        email: String,
+    }
+    struct product {
+        id: i32,
+        name: String,
+        price: i32,
+    }
+    struct order {
+        id: i32,
+        name: String,
+        description: String,
+        cover_img: String,
+
+    }
+}
